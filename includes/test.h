f10b0baf2531608ce1bf3c63abb25d7bcb45e59a
José Luís <juniorrodes@gmail.com>
2022-11-03 00:23:11 -0300

    :tada: First commit

---
+++ includes/test.h
@@ -0,0 +1,11 @@
+//
+// Created by Gamer on 11/2/2022.
+//
+
+#ifndef RAYLIB_TEST_TEST_H
+#define RAYLIB_TEST_TEST_H
+
+[[deprecated]]
+void test();
+
+#endif //RAYLIB_TEST_TEST_H
---
+++ src/test.c
@@ -0,0 +1,8 @@
+//
+// Created by Gamer on 11/2/2022.
+//
+#include "raylib.h"
+
+void test() {
+    TraceLog(LOG_INFO, "test function call");
+}
